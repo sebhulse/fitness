bf0dbad1195b684d0955d9f0766338e4e0176bc3
Sebastian Hulse <seb.hulse@yahoo.co.uk>
2021-04-02 19:35:20 +0100

    finished front end with input parameters

--- web/js/app.js
+++ web/js/app.js
@@ -1,117 +1,315 @@
-(function () {
+// const url = 'https://sebhulse.com/fitness/exercises.json'
+let usedWorkoutExercises = [];
 
-    const quotesEl = document.querySelector('.row');
-    const loaderEl = document.querySelector('.loader');
+async function gatherResponse(response) {
+  return await response.json();
+}
 
-    // get the quotes from API
-    const getQuotes = async (page, limit) => {
-        // var myHeaders = new Headers();
-        // var requestOptions = {
-        //   method: 'GET',
-        //   headers: myHeaders,
-        //   redirect: 'follow'
-        // };
-        const API_URL = `https://api.plantbased.wiki/v1/facts/?page=${page}&limit=${limit}`;
-        const response = await fetch(API_URL);
-        // handle 404
-        if (!response.ok) {
-            throw new Error(`An error occurred: ${response.status}`);
-        }
-        return await response.json();
+// get random number between 0 - max (inclusive)
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// get rest lengths given the workout type and level - returns rest array in the form [restAfterExercise, exerciseDuration, transitionRest]
+function getRest(type, level) {
+  let rest = [];
+  if (type === "cardio" && level === "beginner") {
+    rest = [15, 30, 40];
+  } else if (type === "cardio" && level === "intermediate") {
+    rest = [10, 30, 40];
+  } else if (type === "cardio" && level === "advanced") {
+    rest = [5, 40, 30];
+  } else if (type === "strength" && level === "beginner") {
+    rest = [20, 30, 40];
+  } else if (type === "strength" && level === "intermediate") {
+    rest = [15, 35, 40];
+  } else if (type === "strength" && level === "advanced") {
+    rest = [10, 40, 40];
+  }
+  return rest;
+}
+
+// returns int of how many sections the workout should be split up into dependant on input workoutDuration
+function getNumWorkoutSections(workoutDuration) {
+  let numSections;
+  if (workoutDuration < 840) {
+    numSections = 2;
+  } else if (workoutDuration >= 840 && workoutDuration < 1260) {
+    numSections = getRandomInt(2, 4);
+  } else if (workoutDuration >= 1260 && workoutDuration < 1800) {
+    numSections = getRandomInt(3, 5);
+  } else if (workoutDuration >= 1800) {
+    numSections = getRandomInt(6, 8);
+  }
+  return numSections;
+}
+
+// get a new unique exercise from usedExercises and (available) exercisesIn
+function getNewUniqueExercise(usedExercises, exercisesIn) {
+  do {
+    number = getRandomInt(0, exercisesIn.length);
+    activity = exercisesIn[number];
+  } while (usedExercises.indexOf(activity) >= 0);
+  return activity;
+}
+
+// checks given exercise section e.g. 'warmup' for exercises in given json object with target area e.g. 'upper' and returns array
+function getWarmCoolArea(exercise, area) {
+  let exercises = [];
+  for (var i = 0; i < json[exercise].length; i++) {
+    if (json[exercise][i].area.indexOf(area) >= 0) {
+      exercises.push(json[exercise][i].name);
+    }
+  }
+  return exercises;
+}
+
+// checks workout section for exercises in given json object with given type e.g. 'strength', given level e.g. 'intermediate', given area e.g. 'core' and returns array
+function getWorkoutTypeLevelArea(typeIn, levelIn, areaIn) {
+  let exercises = [];
+  for (var i = 0; i < json.workout.length; i++) {
+    if (
+      json.workout[i].type.indexOf(typeIn) >= 0 &&
+      json.workout[i].level.indexOf(levelIn) >= 0 &&
+      json.workout[i].area.indexOf(areaIn) >= 0
+    ) {
+      exercises.push(json.workout[i].name);
+    }
+  }
+  return exercises;
+}
+
+// strength or cardio
+function generateFunWorkoutSection(sectionDurationIn, exercisesIn, rest) {
+  let restAfterExercise = rest[0];
+  let exerciseDur = rest[1];
+  let transitionRest = rest[2];
+  let bodyDuration = exerciseDur + restAfterExercise;
+  let currentDuration = 0;
+  let activitySection = [];
+
+  do {
+    let exercise = getNewUniqueExercise(usedWorkoutExercises, exercisesIn);
+    usedWorkoutExercises.push(exercise);
+
+    activityBody = {
+      exercise: exercise,
+      duration: exerciseDur,
+    };
+    restBody = {
+      exercise: "rest",
+      duration: restAfterExercise,
+    };
+
+    if (currentDuration === 0) {
+      activitySection.push(activityBody);
+    } else {
+      activitySection.push(restBody);
+      activitySection.push(activityBody);
     }
 
-    // show the quotes
-    const showQuotes = (quotes) => {
-        quotes.forEach(quote => {
-            const quoteEl = document.createElement('div');
-            quoteEl.classList.add('col-sm-6', 'col-lg-4', 'mb-4');
-            // quoteEl.style.position = 'absolute';
-            // quoteEl.style.left = '50%';
-            // quoteEl.style.top = '0px';
+    currentDuration += bodyDuration;
+  } while (currentDuration < sectionDurationIn);
+  transitionBody = {
+    exercise: "transition",
+    duration: transitionRest,
+  };
 
+  activitySection.push(transitionBody);
+  return activitySection;
+}
 
-            // quoteEl.styleList.add('style="position: absolute; left: 50%; top: 0px;"')
-            quoteEl.innerHTML = `
-                <div class="card p-3">
-                    <figure class="p-3 mb-0">
-                        <blockquote class="blockquote">
-                            <p>${quote.content}</p>
-                        </blockquote>
-                        <figcaption class="blockquote-footer mb-0 text-muted">
-                        <a href="${quote.sourceURL}" target="_blank">${quote.sourceTitle}</a>, <i>${quote.sourceAuthor}</i>
+// strength or cardio
+function generateFinisherSection(finisherDuration, rest) {
+  let restAfterExercise = rest[0];
+  let exerciseDur = rest[1];
+  let transitionRest = rest[2];
+  let currentDuration = 0;
+  let activitySection = [];
+  usedExercises = [];
 
-                        </figcaption>
-                    </figure>
-                </div>
-        `;
-
-            quotesEl.appendChild(quoteEl);
-        });
+  do {
+    let exercise = getNewUniqueExercise(usedExercises, usedWorkoutExercises);
+    usedExercises.push(exercise);
+    let activityBody = {
+      exercise: exercise,
+      duration: transitionRest,
     };
 
-    const hideLoader = () => {
-        loaderEl.classList.remove('show');
+    activitySection.push(activityBody);
+    currentDuration += transitionRest;
+  } while (currentDuration < finisherDuration);
+  let transitionBody = {
+    exercise: "transition",
+    duration: transitionRest,
+  };
+
+  activitySection.push(transitionBody);
+
+  return activitySection;
+}
+
+// strength
+function generateSupersetsWorkoutSection() {}
+
+// strength
+function generatePyramidsWorkoutSection() {}
+
+// strength
+function generateRevPyramidsWorkoutSection() {}
+
+// cardio
+function generateIntervalsWorkoutSection() {}
+
+// cardio
+function generateIntervalsWorkoutSection() {}
+
+// takes sectionDuration, exercises array, numWorkoutSections and rest array, chooses workout type, generates main body of workout and returns json
+function generateWorkoutSection(
+  workoutDuration,
+  workoutExercisesIn,
+  numWorkoutSections,
+  rest
+) {
+  let workoutType;
+  let workoutSectionDuration = workoutDuration / numWorkoutSections;
+  let activitySection = [];
+
+  for (var i = 0; i < numWorkoutSections; i++) {
+    let workoutSection = generateFunWorkoutSection(
+      workoutSectionDuration,
+      workoutExercisesIn,
+      rest
+    );
+
+    activitySection.push(workoutSection);
+  }
+  return activitySection;
+}
+
+// takes sectionDuration, exercises array, section and rest array, generates warmup or cooldown of workout and returns json
+function generateWarmCoolSection(
+  sectionDurationIn,
+  exercisesIn,
+  section,
+  rest
+) {
+  let exerciseDur = rest[1];
+  let transitionRest = rest[2];
+  let currentDuration = 0;
+  let activitySection = [];
+  let usedExercises = [];
+
+  do {
+    let exercise = getNewUniqueExercise(usedExercises, exercisesIn);
+    usedExercises.push(exercise);
+    let activityBody = {
+      exercise: exercise,
+      duration: exerciseDur,
     };
 
-    const showLoader = () => {
-        loaderEl.classList.add('show');
+    activitySection.push(activityBody);
+    currentDuration += exerciseDur;
+  } while (currentDuration < sectionDurationIn);
+  if (section === "warmup") {
+    let transitionBody = {
+      exercise: "transition",
+      duration: transitionRest,
     };
 
-    const hasMoreQuotes = (page, limit, total) => {
-        const startIndex = (page - 1) * limit + 1;
-        return total === 0 || startIndex < total;
-    };
+    activitySection.push(transitionBody);
+  }
+  return activitySection;
+}
 
-    // load quotes
-    const loadQuotes = async (page, limit) => {
+// takes duration and exercises and calls section generation functions, returns json with complete workout
+function generateWorkout(
+  durationIn,
+  warmupExercisesIn,
+  workoutExercisesIn,
+  cooldownExercisesIn,
+  rest
+) {
+  let warmupDuration = Math.floor(durationIn * 0.1);
+  // console.log(warmupDuration)
+  let cooldownDuration = warmupDuration;
+  let finisherDuration = Math.floor(durationIn * 0.1);
+  let workoutDuration =
+    durationIn - warmupDuration - cooldownDuration - finisherDuration;
+  let numWorkoutSections = getNumWorkoutSections(workoutDuration);
+  let section;
 
-        // show the loader
-        showLoader();
+  // this will be passed to generateFinisherSection() to generate finisher based on exercises already done
+  section = "warmup";
+  let warmup = generateWarmCoolSection(
+    warmupDuration,
+    warmupExercisesIn,
+    section,
+    rest
+  );
+  let workout = generateWorkoutSection(
+    workoutDuration,
+    workoutExercisesIn,
+    numWorkoutSections,
+    rest
+  );
+  let finisher = generateFinisherSection(finisherDuration, rest);
+  section = "cooldown";
+  let cooldown = generateWarmCoolSection(
+    cooldownDuration,
+    cooldownExercisesIn,
+    section,
+    rest
+  );
+  let body = {
+    warmup: warmup,
+    workout: workout,
+    finisher: finisher,
+    cooldown: cooldown,
+  };
+  return body;
+}
 
-        // 0.5 second later
-        setTimeout(async () => {
-            try {
-                // if having more quotes to fetch
-                if (hasMoreQuotes(page, limit, total)) {
-                    // call the API to get quotes
-                    const response = await getQuotes(page, limit);
-                    // show quotes
-                    showQuotes(response);
-                    // update the total
-                    total = 100;
-                }
-            } catch (error) {
-                console.log(error.message);
-            } finally {
-                hideLoader();
-            }
-        }, 500);
+async function handleRequest() {
+  const init = {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+  };
 
-    };
+  // const { searchParams } = new URL(request.url)
+  let type = "cardio";
+  let level = "intermediate";
+  let area = "upper";
+  let duration = 2400;
+  console.log(duration);
+  const url = `https://api.sebhulse.com/filter/?type=${type}&level=${level}&area=${area}`;
 
-    // control variables
-    let currentPage = 1;
-    const limit = 10;
-    let total = 0;
+  const response = await fetch(url, init, "jsonp");
+  const results = await gatherResponse(response);
+  // console.log(results)
 
+  let rest = getRest(type, level);
+  console.log(rest);
+  // filter the exercises according to inputs
+  let warmup_area_exercises = results.warmup;
+  let workout_exercises = results.workout;
+  let cooldown_area_exercises = results.cooldown;
 
-    window.addEventListener('scroll', () => {
-        const {
-            scrollTop,
-            scrollHeight,
-            clientHeight
-        } = document.documentElement;
+  let generatedWorkout = generateWorkout(
+    duration,
+    warmup_area_exercises,
+    workout_exercises,
+    cooldown_area_exercises,
+    rest
+  );
+  // console.log(generatedWorkout)
+  // let body = {
+  //   message: 'this worked',
+  // }
+  // console.log(generateWorkout)
 
-        if (scrollTop + clientHeight >= scrollHeight - 5 &&
-            hasMoreQuotes(currentPage, limit, total)) {
-            currentPage++;
-            loadQuotes(currentPage, limit);
-        }
-    }, {
-        passive: true
-    });
-
-    // initialize
-    loadQuotes(currentPage, limit);
-
-})();
+  let generatedWorkout1 = JSON.stringify(generatedWorkout);
+  console.log(generatedWorkout1);
+  // return new Response(generatedWorkout1, init)
+}
