21f0cc1bd5b58bd501f17b59ed15163433859f08
Sebastian Hulse <seb.hulse@yahoo.co.uk>
2021-04-13 23:48:04 +0100

    refactored workout breakdown dynamic html

--- web/js/view.js
+++ web/js/view.js
@@ -50,21 +50,18 @@ function getParameters() {
 
 // create accordion element
 async function buildAccordion(jsonWorkout) {
-  // const jsonWorkout = await JSON.parse(workout);
-  console.log(jsonWorkout);
 
   // for each section in the whole json (warmup, cooldown etc), create the body text
-  Object.keys(jsonWorkout).forEach(function (key) {
+  Object.keys(jsonWorkout).forEach(function(key) {
     if (key != "parameters") {
       let name = key.charAt(0).toUpperCase() + key.slice(1);
-      let bodyShell = `<ul class="list-group list-group-flush">`;
 
-      // let ul = document.createElement("ul");
-      // ul.className = "list-group list-group-flush";
-      // ul.id = "listContainerWorkout";
+      let ul = document.createElement("ul");
+      ul.className = "list-group list-group-flush";
+      ul.id = "listContainerWorkout";
 
       // for each exercises 'blob' section in each section
-      Object.keys(jsonWorkout[key]).forEach(function (key1) {
+      Object.keys(jsonWorkout[key]).forEach(function(key1) {
         let exercise = jsonWorkout[key][key1]["exercise"];
         let durationWC = jsonWorkout[key][key1]["duration"];
         if (exercise === "rest") {
@@ -72,27 +69,25 @@ async function buildAccordion(jsonWorkout) {
         } else if (exercise === "transition") {
           exercise = "Section Transition";
         }
+
         // checks if there's an array and if so, loops through the array to display exercises
         if (typeof exercise !== "undefined") {
-          let bodyListItem = `<li class="list-group-item d-flex justify-content-between">${exercise}
-          <small class="text-muted">${durationWC}s</small>
-          </li>`;
-          bodyShell += bodyListItem;
 
-          // let bodyListItem = document.createElement("li");
-          // bodyListItem.className =
-          //   "list-group-item d-flex justify-content-between";
-          // bodyListItem.innerHTML = `${exercise}`;
-          // bodyListItem.id = "bodyListItem";
-          //
-          // let durationSmall = document.createElement("small");
-          // durationSmall.className = "text-muted";
-          // durationSmall.innerHTML = `${durationWC}s`;
-          // durationSmall.id = "durationSmall";
-          //
-          // bodyListItem.appendChild(durationSmall);
-          //
-          // ul.appendChild(bodyListItem);
+          let bodyListItem = document.createElement("li");
+          bodyListItem.className =
+            "list-group-item d-flex justify-content-between";
+          bodyListItem.innerHTML = `${exercise}`;
+          bodyListItem.id = "bodyListItem";
+
+          let durationSmall = document.createElement("small");
+          durationSmall.className = "text-muted";
+          durationSmall.innerHTML = `${durationWC}s`;
+          durationSmall.id = "durationSmall";
+
+          bodyListItem.appendChild(durationSmall);
+
+          ul.appendChild(bodyListItem);
+
         } else {
           let exerciseArray = jsonWorkout[key][key1];
           for (var i = 0; i < exerciseArray.length; i++) {
@@ -103,93 +98,69 @@ async function buildAccordion(jsonWorkout) {
               arrayExercise = "Section Transition";
             }
             let exDuration = exerciseArray[i].duration;
-            let bodyListItem = `<li class="list-group-item d-flex justify-content-between">${arrayExercise}
-              <small class="text-muted">${exDuration}s</small>
-            </li>`;
-            bodyShell += bodyListItem;
 
-            // let bodyListItem2 = document.createElement("li");
-            // bodyListItem2.className =
-            //   "list-group-item d-flex justify-content-between";
-            // bodyListItem2.innerHTML = `${arrayExercise}`;
-            // bodyListItem2.id = "bodyListItem2";
-            //
-            // let durationSmall2 = document.createElement("small");
-            // durationSmall2.className = "text-muted";
-            // durationSmall2.innerHTML = `${exDuration}s`;
-            // durationSmall2.id = "durationSmall2";
-            //
-            // bodyListItem2.appendChild(durationSmall2);
-            //
-            // ul.appendChild(bodyListItem2);
+            let bodyListItem2 = document.createElement("li");
+            bodyListItem2.className =
+              "list-group-item d-flex justify-content-between";
+            bodyListItem2.innerHTML = `${arrayExercise}`;
+            bodyListItem2.id = "bodyListItem2";
+
+            let durationSmall2 = document.createElement("small");
+            durationSmall2.className = "text-muted";
+            durationSmall2.innerHTML = `${exDuration}s`;
+            durationSmall2.id = "durationSmall2";
+
+            bodyListItem2.appendChild(durationSmall2);
+
+            ul.appendChild(bodyListItem2);
           }
         }
       });
-      bodyShell += `</ul>`;
-      // console.log(ul);
 
       let iterator = `iterator${key}`;
 
-      // create the accordion shell element
-      let accordion = `<div class="accordion-item" id="accordionOne">
-              <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${iterator}" aria-expanded="true" aria-controls="${iterator}">
-                  ${name}
-                </button>
-              </h2>
-              <div id="${iterator}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionPlaceholder">
-                <div class="accordion-body col-md-8 mx-auto">
-                  ${bodyShell}
-                </div>
-              </div>
-            </div>`;
-      document
-        .getElementById("accordionPlaceholder")
-        .insertAdjacentHTML("beforebegin", accordion);
+      let accordionDiv = document.createElement("div");
+      accordionDiv.className = "accordion-item";
+      accordionDiv.id = "accordionOne";
 
-      // let accordionDiv = document.createElement("div");
-      // accordionDiv.className = "accordion-item";
-      // accordionDiv.id = "accordionOne";
-      //
-      // let accordionHeader = document.createElement("h2");
-      // accordionHeader.className = "accordion-header";
-      // accordionHeader.id = "headingOne";
-      //
-      // let accordionButton = document.createElement("button");
-      // accordionButton.className = "accordion-button collapsed";
-      // accordionButton.type = "button";
-      // accordionButton.setAttribute("data-bs-toggle", "collapse");
-      // accordionButton.setAttribute("data-bs-target", iterator);
-      // accordionButton.setAttribute("aria-expanded", "true");
-      // accordionButton.setAttribute("aria-controls", iterator);
-      // accordionButton.innerHTML = name;
-      //
-      // let accordionDiv2 = document.createElement("div");
-      // accordionDiv2.className = "accordion-collapse collapse";
-      // accordionDiv2.id = iterator;
-      // accordionDiv2.setAttribute("aria-labelledby", "headingOne");
-      // accordionDiv2.setAttribute("data-bs-parent", "#accordionPlaceholder");
-      //
-      // let accordionDiv3 = document.createElement("div");
-      // accordionDiv3.className = "accordion-body col-md-8 mx-auto";
-      //
-      //
-      // accordionDiv.appendChild(accordionHeader);
-      // accordionHeader.appendChild(accordionButton);
-      // accordionDiv.appendChild(accordionDiv2);
-      // accordionDiv2.appendChild(accordionDiv3);
-      // accordionDiv3.appendChild(ul);
-      // console.log(accordionDiv);
-      //
-      // let accordionPlaceholder = document.getElementById("accordionPlaceholder");
-      // accordionPlaceholder.appendChild(accordionDiv);
+      let accordionHeader = document.createElement("h2");
+      accordionHeader.className = "accordion-header";
+      accordionHeader.id = "headingOne";
+
+      let accordionButton = document.createElement("button");
+      accordionButton.className = "accordion-button collapsed";
+      accordionButton.type = "button";
+      accordionButton.setAttribute("data-bs-toggle", "collapse");
+      accordionButton.setAttribute("data-bs-target", `#${iterator}`);
+      accordionButton.setAttribute("aria-expanded", "true");
+      accordionButton.setAttribute("aria-controls", iterator);
+      accordionButton.innerHTML = name;
+
+      let accordionDiv2 = document.createElement("div");
+      accordionDiv2.className = "accordion-collapse collapse";
+      accordionDiv2.id = iterator;
+      accordionDiv2.setAttribute("aria-labelledby", "headingOne");
+      accordionDiv2.setAttribute("data-bs-parent", "#accordionPlaceholder");
+
+      let accordionDiv3 = document.createElement("div");
+      accordionDiv3.className = "accordion-body col-md-8 mx-auto";
+
+
+      accordionDiv.appendChild(accordionHeader);
+      accordionHeader.appendChild(accordionButton);
+      accordionDiv.appendChild(accordionDiv2);
+      accordionDiv2.appendChild(accordionDiv3);
+      accordionDiv3.appendChild(ul);
+      console.log(accordionDiv);
+
+      let accordionPlaceholder = document.getElementById("accordionPlaceholder");
+      accordionPlaceholder.appendChild(accordionDiv);
     }
   });
 }
 
-//
+// create history element
 function buildHistory() {
-  // let listShell = `<ol class="list-group">`;
   let keys = [];
   for (var i = 0; i < sessionStorage.length; i++) {
     keys.push(sessionStorage.key(i));
